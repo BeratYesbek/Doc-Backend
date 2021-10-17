30349c71e9b5ff0b36891f0a5a7454a7b7c6cd82
mdd <dedemeih.c@gmail.com>
2021-10-17 21:19:39 +0300

    ekim17

--- src/main/java/com/doc/doc_backend/core/utilities/concretes/DataResult.java
+++ src/main/java/com/doc/doc_backend/core/utilities/concretes/DataResult.java
@@ -1,7 +1,8 @@
 package com.doc.doc_backend.core.utilities.concretes;
 
 public class DataResult<T> extends Result {
-
+    //mdd was here
+    int num;
     private T data;
 
     public DataResult(T data, boolean success, String message) {
